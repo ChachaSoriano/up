4d962e5f23c3b9911507a84981024241cd3499c1
ChachaSoriano <chaSoriano12@gmail.com>
2015-01-03 19:10:30 +0800

    9

--- faculty/enlistment/functions/change.php
+++
@@ -1,4 +0,0 @@
-<?php include '../../../config.php' ?>
-<?php
-	mysql_query("update renlistmentschedule set ay='".filt($_POST['academicyear'])."',sem='".filt($_POST['semester'])."',start='".filt(date("Y-m-d",strtotime($_POST['start'])))."',end='".filt(date("Y-m-d",strtotime($_POST['end'])))."'");
-?>
